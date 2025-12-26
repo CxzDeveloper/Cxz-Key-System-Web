8be00223479670a59cf38722b561bec34fe69798
CxzDeveloper <krisnaokeh@gmail.com>
2025-12-27 04:54:17 +0700

    Simplify key generation using Math.random
    Replaced crypto-based key generation with random string method.

--- pages/api/generate-key.js
+++ pages/api/generate-key.js
@@ -1,15 +1,4 @@
-import crypto from 'crypto';
-
-export const KEYS = global.keys || (global.keys = []);
-
 export default function handler(req, res) {
-  const key = 'CXZ-' + crypto.randomBytes(5).toString('hex').toUpperCase();
-  const expires = Date.now() + 2 * 24 * 60 * 60 * 1000;
-
-  KEYS.push({ key, expires });
-
-  res.json({
-    key,
-    expires: new Date(expires).toLocaleString()
-  });
+  const key = "CXZ-" + Math.random().toString(36).substring(2, 12).toUpperCase()
+  res.status(200).json({ key })
 }
