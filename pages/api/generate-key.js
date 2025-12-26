7faa6c4040a3c3d5e9ee94a7bfd822f3745c2196
CxzDeveloper <krisnaokeh@gmail.com>
2025-12-27 04:26:19 +0700

    Add API endpoint to generate a unique key

--- pages/api/generate-key.js
+++ pages/api/generate-key.js
@@ -1,1 +1,15 @@
+import crypto from 'crypto';
 
+export const KEYS = global.keys || (global.keys = []);
+
+export default function handler(req, res) {
+  const key = 'CXZ-' + crypto.randomBytes(5).toString('hex').toUpperCase();
+  const expires = Date.now() + 2 * 24 * 60 * 60 * 1000;
+
+  KEYS.push({ key, expires });
+
+  res.json({
+    key,
+    expires: new Date(expires).toLocaleString()
+  });
+}
