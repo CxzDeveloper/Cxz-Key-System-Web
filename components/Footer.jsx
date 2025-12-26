410f6d4dd56f25ca64e51815a7abd1c13342afc4
CxzDeveloper <krisnaokeh@gmail.com>
2025-12-27 04:36:16 +0700

    Add Footer component with copyright notice

---
+++ components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="w-full bg-gray-100 py-6 mt-10 text-center text-gray-500">
+      © 2025 Cxz Developer. All rights reserved.
+    </footer>
+  );
+}
